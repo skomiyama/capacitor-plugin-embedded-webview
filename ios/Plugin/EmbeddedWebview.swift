f8bc13a8d9c041bdc2e79f814c43ca7ebc0aaf2c
skomiyama <s.komiyama@twogate.com>
2022-07-18 14:44:45 +0900

    ios methods

--- ios/Plugin/EmbeddedWebview.swift
+++ ios/Plugin/EmbeddedWebview.swift
@@ -1,8 +1,68 @@
 import Foundation
+import Capacitor
+import WebKit
 
-@objc public class EmbeddedWebview: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+@objc class EmbeddedWebview: UIViewController, WKNavigationDelegate, WKUIDelegate {
+    private func deviceRect(x: CGFloat, y: CGFloat) -> CGRect {
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
+        return CGRect(x: x, y: y, width: width, height: height - 84)
+    }
+    
+    private var url: URL!
+    private var css: String?
+    private var webView: WKWebView!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    convenience init(url: String, css: String = "") {
+        self.init(nibName:nil, bundle:nil)
+        self.url = URL(string:url)
+        self.css = css
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        let frame = deviceRect(x: 0, y: 0)
+        webView = WKWebView(frame: frame, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.webView.uiDelegate = self
+        self.webView.navigationDelegate = self
+    }
+    
+    // target is _blank
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+       if navigationAction.targetFrame == nil {
+           if  let url = navigationAction.request.url {
+               UIApplication.shared.open(url, options: [:], completionHandler: nil)
+           }
+       }
+       return nil
+    }
+    
+    public func create(url: String) {
+        let URL = URL(string:url)
+        let request = URLRequest(url: URL!)
+        webView.load(request)
+    }
+
+    public func destroy() {
+        self.dismiss(animated: false)
+    }
+
+    public func show() {
+        view.isHidden = false
+    }
+    public func hide() {
+        view.isHidden = true
     }
 }
---
+++ ios/Plugin/WebviewController.swift
@@ -0,0 +1,12 @@
+//
+//  WebviewController.swift
+//  Plugin
+//
+//  Created by Shota Komi on 2022/06/24.
+//  Copyright © 2022 Max Lynch. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+
