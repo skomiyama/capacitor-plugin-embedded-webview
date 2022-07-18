b0d0b3b2029d50dc818e6206c2a9cdf255ea3025
skomiyama <s.komiyama@twogate.com>
2022-07-18 14:43:39 +0900

    basic embedded webview methods

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,23 @@
+export interface WebviewConfiguration {
+  width: number;
+  height: number;
+  enableCookie?: boolean;
+}
+
+export interface EmbeddedWebviewOptions {
+  url: string;
+  webviewConfiguration: WebviewConfiguration;
+}
+
 export interface EmbeddedWebviewPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  create(options: EmbeddedWebviewOptions): Promise<void>;
+  hide(): Promise<EmbeddedWebviewVisibility>;
+  show(): Promise<EmbeddedWebviewVisibility>;
+  dismiss(): Promise<void>;
+}
+
+
+export interface EmbeddedWebviewVisibility {
+  visibility: boolean;
 }
